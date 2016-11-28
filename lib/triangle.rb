e38c3728109d5ad833558de5e9d343d7355af1d8
Bill Klinkow and Margie Nevarez <margie@>
2016-11-28 09:21:06 -0800

    added two more specs and built type method to includeisosceles and not a triangle outputs

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,10 +6,14 @@ class Triangle
   end
 
   define_method(:type) do
-    if (@side1 == @side2) && (@side1 == @side3)
+    if (@side1 + @side2 <= @side3) || (@side3 + @side2 <= @side1) || (@side1 + @side3 <= @side2)
+      return "not a triangle"
+    elsif (@side1 == @side2) && (@side1 == @side3)
       return "equilateral"
     elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
       return "scalene"
+    elsif (@side1 == @side2 && @side1 != @side3) || (@side3 == @side1 && @side3 != @side2) || (@side2 == @side3 && @side2 != @side1)
+      return "isosceles"
     end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -12,5 +12,16 @@ describe(Triangle) do
       test_triangle = Triangle.new(3, 2, 4.5)
       expect(test_triangle.type()).to(eq("scalene"))
     end
+
+    it ("returns isosceles if two sides are equal") do
+      test_triangle = Triangle.new(2, 2, 3)
+      expect(test_triangle.type()).to(eq("isosceles"))
+    end
+
+    it ("returns 'not a triangle' if the sides cannot form a triangle") do
+      test_triangle = Triangle.new(2, 2, 10)
+      expect(test_triangle.type()).to(eq("not a triangle"))
+    end
+
   end
 end
